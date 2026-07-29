267d9950283e626ac097a0e7cfaecc71ef433d69
Elevate水野蒼良 <ateelevate@gmail.com>
2026-07-29 20:06:57 +0900

    Connect snack diagnosis to Custom GPT

--- snack-personality-result/config.js
+++ snack-personality-result/config.js
@@ -1,4 +1,5 @@
-const CUSTOM_GPT_URL = "";
+const CUSTOM_GPT_URL =
+  "https://chatgpt.com/g/g-6a69900e27e0819186b2d9fabf803279-oguo-zi-xing-ge-zhen-duan-hao-kinaoguo-zi-tewakaru12taihu";
 
 window.SNACK_CONFIG = Object.freeze({
   CUSTOM_GPT_URL,
